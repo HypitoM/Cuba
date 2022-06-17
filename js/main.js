c71b76f4d844ba0aa0e7408fdc186b4a3f8b73a8
HypitoM <boba.zdez@yandex.ru>
2022-06-17 17:17:05 +0300

    prepare project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function (){
-
+    console.log("andrei_xui")
     $('.photo__slider').slick({
+        slidesToShow: 1,
         arrows: false,
         prevArrow: '<button type = "button" class = "slick-prev" ><img src="images/arrow-left.svg" alt="">< /button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
